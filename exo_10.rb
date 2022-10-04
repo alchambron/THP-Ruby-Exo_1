f15910397c3ecf1b930b80f1b2f22b0bc5450cf7
alchambron <alexis.chambron@gmail.com>
2022-10-04 16:37:59 +0200

    fixing exo_10

--- exo_10.rb
+++ exo_10.rb
@@ -1,11 +1,11 @@
-print 'Quel est votre année de naissance ? '
+print 'Quelle est votre année de naissance ? '
 ybirth = gets.chomp.to_i
 currentyear = Time.now.year
 year = 0
 
 while ybirth <= currentyear
   print "En #{ybirth} tu "
-  puts "avait #{year} ans"
+  puts "avais #{year} ans"
   ybirth += 1
   year += 1
 end
